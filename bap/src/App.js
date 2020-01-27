02480c5fc85a85f35badea618c6eb07c721021cc
Kanzi Louagie <kanzi.louagie@student.howest.be>
2020-01-27 01:00:20 +0100

    login redone

--- bap/src/App.js
+++ bap/src/App.js
@@ -1,40 +1,25 @@
-import React, { Component } from 'react'
-import fire from './config/Fire';
-import Login from './Login';
-import Home from './Home';
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {},
-    }
-  }
-
-  componentDidMount() {
-    this.authListener();
-  }
-
-  authListener() {
-    fire.auth().onAuthStateChanged((user) => {
-      console.log(user);
-      if(user) {
-        this.setState({user});
-        // localStorage.setItem('user', user.uid);
-      } else {
-        this.setState({user: null});
-        // localStorage.removeItem('user');
-      }
-    });
-  }
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from "./Home";
+import Login from "./Authentication/Login";
+import SignUp from "./Authentication/SignUp";
+import { AuthProvider } from './Authentication/Auth';
+import PrivateRoute from './Authentication/PrivateRoute';
 
 
-  render() {
-    return (
-      <div className="App">
-    {this.state.user ? (<Home />) : (<Login />)}
-      </div>
-    )
-  }
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <div>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} />
+        </div>
+      </Router>
+    </AuthProvider>
+  )
 }
-export default App;
+
+export default App
--- bap/src/Home.js
+++ bap/src/Home.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react';
+import app from './Authentication/base';
 
-class Home extends Component {
-
-  constructor(props) {
-    super(props);
-  }
-
-
-  render() {
-    return (
-      <div className="col-md-6">
-        <h1>You are home.</h1>
-      </div>
-    )
-  }
+function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <button onClick={() => app.auth().signOut()}>Sign out</button>
+    </>
+  )
 }
 
 export default Home;
