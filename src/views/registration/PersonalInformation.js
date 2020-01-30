fce001a5f6d54bd7de6a075322c949d3e248106d
Kanzi Louagie <kanzi.louagie@student.howest.be>
2020-01-30 16:02:37 +0100

    database connected and functional

--- src/views/registration/PersonalInformation.js
+++ src/views/registration/PersonalInformation.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router';
+import firebase from '../../authentication/base';
 
 const PersonalInformation = () => {
+  const history = useHistory();
+  const handleSignUp = useCallback(
+    async event => {
+      event.preventDefault();
+      const { email, password, username, repeat } = event.target.elements;
+      if (repeat.value === password.value) {
+        try {
+          await firebase
+            .auth()
+            .createUserWithEmailAndPassword(email.value, password.value);
+          firebase
+            .firestore()
+            .collection('users')
+            .add({
+              username: username.value,
+              email: email.value
+            });
+          history.push('/step4');
+        } catch (error) {
+          alert(error);
+        }
+      } else {
+        alert("passwords don't match");
+      }
+    },
+    [history]
+  );
+
   return (
     <>
       <h1>PersonalInformation</h1>
-      <button>
-        <Link to="/step4">Step4</Link>
-      </button>
+      <form onSubmit={handleSignUp}>
+        <label>
+          Gebruikersnaam
+          <input name="username" type="text" placeholder="johnsmith12" />
+        </label>
+        <label>
+          E-mailadres
+          <input name="email" type="email" placeholder="example@info.be" />
+        </label>
+        <label>
+          Kies een wachtwoord
+          <input
+            name="password"
+            type="password"
+            placeholder="Wat is je wachtwoord?"
+          />
+        </label>
+        <label>
+          Herhaal je wachtwoord
+          <input
+            name="repeat"
+            type="password"
+            placeholder="Wat is je wachtwoord?"
+          />
+        </label>
+        <button type="submit">Step4</button>
+      </form>
     </>
   );
 };
